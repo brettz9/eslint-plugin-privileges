ff92debcdf9a5746ad11029a1314558fe467d53e
Brett Zamir <brettz9@yahoo.com>
2020-03-07 22:41:34 +0800

    - Todo

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const defaultKeys = require('eslint-visitor-keys');
 console.log('defaultKeys', defaultKeys);
 */
 
-// Todo: Should probably just use esquery instead
+// Todo: Aggregate results into a file `Map`, and then perform linting (or
+//  if doing along the way, only perform linting once per discovered file).
+//  But ensure the traversal code is separated so we can have a useful
+//  generic traverser by import/require.
+
+// Todo: can probably just use esquery instead for traversal?
 
 // Decided againts @babel/traverse, in case might use ESLint AST
 //  for ESLint rules
